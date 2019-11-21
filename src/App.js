11908c4f47f6e7bcabcc7fc912ba4cbbc5c3c356
chenwen6498 <chenwen6498@163.com>
2019-11-21 19:13:07 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Avatar, Table, Spin, Icon } from "antd";
+import { getAllStudents } from "./client";
+import { Container } from "./Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+
+class App extends React.Component {
+  state = {
+    students: [],
+    isFetching: false
+  };
+
+  componentDidMount() {
+    this.fetchStudents();
+    this.setState({ isFetching: true });
+  }
+
+  fetchStudents = () => {
+    getAllStudents()
+      .then(res => res.json())
+      .then(students => this.setState({ students, isFetching: false }));
+  };
+
+  render() {
+    const { students, isFetching } = this.state;
+    const columns = [
+      {
+        title: "Avatar",
+        key: "avatar",
+        render(text, row, index) {
+          return (
+            <div>
+              <Avatar size="large">{`${row.firstName.charAt(0).toUpperCase() +
+                row.lastName.charAt(0).toUpperCase()}`}</Avatar>
+            </div>
+          );
+        }
+      },
+      {
+        title: "Student Id",
+        dataIndex: "studentId",
+        key: "studentId"
+      },
+      {
+        title: "First Name",
+        dataIndex: "firstName",
+        key: "firstName"
+      },
+      {
+        title: "Last Name",
+        dataIndex: "lastName",
+        key: "lastName"
+      },
+      {
+        title: "Email",
+        dataIndex: "email",
+        key: "email"
+      },
+      {
+        title: "Gender",
+        dataIndex: "gender",
+        key: "gender"
+      }
+    ];
+
+    if (isFetching) {
+      return (
+        <Container>
+          <Spin indicator={antIcon} />
+        </Container>
+      );
+    }
+
+    if (students && students.length) {
+      return (
+        <Container>
+          <Table dataSource={students} columns={columns} rowKey="studentId" />
+        </Container>
+      );
+    }
+    return <h1>No students!</h1>;
+  }
 }
 
 export default App;
